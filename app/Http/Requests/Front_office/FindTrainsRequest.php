a74fd4fead8f283f90b70e4fad1c36cb5cea0e7a
ait ouaguerd anass <aitougeurdanass@gmail.com>
2024-03-21 22:51:09 +0000

    les contrôleurs de la validation des requestes

---
+++ app/Http/Requests/Front_office/FindTrainsRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Front_office;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FindTrainsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'gare_depart' => 'required',
+            'gare_distination' => 'required',
+            'date_depart' => 'required|date',
+            'date_retour' => 'required|date',
+        ];
+    }
+}
