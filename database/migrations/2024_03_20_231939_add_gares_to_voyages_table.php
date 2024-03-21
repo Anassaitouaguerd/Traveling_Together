f923e1b7a62ad9c86c30bd87529cde5e9be7a11c
ait ouaguerd anass <aitougeurdanass@gmail.com>
2024-03-21 22:57:02 +0000

    les nouveaux changement migration du base du donnée

---
+++ database/migrations/2024_03_20_231939_add_gares_to_voyages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('voyages', function (Blueprint $table) {
+            $table->unsignedBigInteger('gare_depart');
+            $table->unsignedBigInteger('gare_arrivee');
+            $table->foreign('gare_depart')->references('id')->on('gares')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('gare_arrivee')->references('id')->on('gares')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('voyages', function (Blueprint $table) {
+            //
+        });
+    }
+};
