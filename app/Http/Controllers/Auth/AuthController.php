2cb0e1505c930edb5bd5259f0c5fe6898151a472
ait ouaguerd anass <aitougeurdanass@gmail.com>
2024-04-12 21:35:16 +0000

    the chnange in the methode to register

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Back_office\Users\AddUserRequest;
+use App\Http\Requests\Back_office\Users\LoginUserRequest;
+use App\Http\Requests\PasswordRequest;
+use App\Mail\ForgetPass;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
@@ -15,14 +21,8 @@ class AuthController extends Controller
     {
         $this->user = new User();
     }
-    public function newSingup(Request $request)
+    public function newSingup(AddUserRequest $request)
     {
-        $request->validate([
-            'name' => 'required|min:4',
-            'email' => 'email|required|min:4',
-            'tele' => 'regex:/^\+?[0-9]{10,13}$/',
-            'password' => 'required|min:6|max:16|confirmed',
-        ]);
         $this->user->name = $request->name;
         $this->user->email = $request->email;
         $this->user->tel = $request->tele;
@@ -31,15 +31,10 @@ class AuthController extends Controller
         $request->session()->put('user_name', $this->user->name);
         return redirect('/index')->with('signup', 'Welcom,');
     }
-    public function login(Request $request)
+    public function login(LoginUserRequest $request)
     {
-        $attributes = $request->validate([
-            'email' => ['email', 'required', 'min:4'],
-            'password' => ['required', 'max:16', 'min:6'],
-        ]);
-        $user = User::where('email', $attributes['email'])->first();
-
-        if ($user && Hash::check($attributes['password'], $user->password)) {
+        $user = User::where('email', $request->email)->first();
+        if ($user && Hash::check($request->password, $user->password)) {
             $request->session()->put('user_id', $user->id);
             $request->session()->put('user_name', $user->name);
             $request->session()->put('user_role', $user->role);
@@ -61,5 +56,32 @@ class AuthController extends Controller
         $request->validate([
             'email' => 'required|email|min:4'
         ]);
+        $user = User::where('email', $request->email)->first();
+        if ($user) {
+            $user->remember_token = Str::random(45);
+            $user->save();
+            Mail::to($user)->send(new ForgetPass($user));
+            return back()->with('success', 'Checke your Email');
+        }
+        return back()->with('errore', "this account is not found ");
+    }
+    public function resetPage($token)
+    {
+        $user = User::where('remember_token', $token)->first();
+        if ($user) {
+            return view('Front-office.Forgot-password.reset');
+        }
+        return abort(404);
+    }
+    public function resetPassword(PasswordRequest $request, $token)
+    {
+        $user = User::where('remember_token', $token)->first();
+        if ($user) {
+            $user->remember_token = Str::random(45);
+            $user->password = $request->password;
+            $user->save();
+            return redirect('/login')->with('success', 'Your Password is changed successful');
+        }
+        return back()->with('errore', 'the user is not found');
     }
 }
