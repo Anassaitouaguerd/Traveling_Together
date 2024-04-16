29ce177861b2cf208b63385b1c63a9b13de2c534
ait ouaguerd anass <aitougeurdanass@gmail.com>
2024-04-16 01:02:29 +0100

    partials for the tags and the annyting be fixet into the page in the how and the part reservations

---
+++ resources/views/Front-office/partials/Home/_Header.blade.php
@@ -0,0 +1,34 @@
+<header>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-gradient ">
+        <div class="container-fluid d-flex justify-content-between">
+            <a class="navbar-brand p-0 " href="#">
+                <img src="assets/img/logo_travle.png" alt="travling together logo" style="width: 150px">
+            </a>
+            <button onclick="makeaMargin(this)" class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav mb-2 mb-lg-0">
+
+                    <li class="nav-item">
+                        <a class="nav-link active py-0 px-3 " id="login" aria-current="page" href="/login"
+                            style="font-weight: 500;">
+                            <span>Log in</span>
+                        </a>
+                    </li>
+                    <li class="vr" style="background-color: var(--dark-blue);width: 1px ;"></li>
+                    <li class="nav-item">
+                        <a class="nav-link active py-0 px-3" aria-current="page" href="/sign-up" id="sign_up">
+                            <span
+                                style="background-color: var(--aqua);color: white;padding: 7px 25px;border-radius: 25px;font-weight: 500;">Sign
+                                up</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ resources/views/Front-office/partials/Reservation/_tags_html.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="/assets/css/style2.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+</head>
+
+<body>
+@yield('content')
+</body>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#example').DataTable();
+    });
+</script>
+<script>
+    document.forms.namedItem("add_train_form").addEventListener('submit', function(e) {
+        let gare_entred = $("#id_holder").val();
+        if (gare_entred == "") {
+            e.preventDefault();
+            alert("invalid gare identiant");
+            return;
+        }
+        //alert(isGareExist(gare_entred))
+        isGareExist(gare_entred, "../handlers/garehandler.php").then(data => {
+            if (!data) {
+                e.preventDefault();
+                alert("invalid gare identiant");
+            }
+        })
+
+    })
+</script>
+<script src="../../assets/js/main2.js"></script>
+
+</html>
