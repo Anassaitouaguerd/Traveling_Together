661e9ec11c1487aa503815a075cc70db3794f71a
ait ouaguerd anass <aitougeurdanass@gmail.com>
2024-04-16 01:04:59 +0100

    Add the method to find all trains along with their voyages.

--- app/Http/Controllers/Front_office/SearchController.php
+++ app/Http/Controllers/Front_office/SearchController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Front_office;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Front_office\FindTrainsRequest;
+use App\Models\Gares;
+use App\Models\Trains;
+use App\Models\Voyages;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
@@ -26,8 +29,23 @@ class SearchController extends Controller
         }
         echo json_encode([$AllGareDepart, $AllGareDistination]);
     }
+
     public function FindTrains(FindTrainsRequest $request)
     {
-        dd($request);
+        $point_depart = $request->gare_depart;
+        $point_distination = $request->gare_distination;
+
+        $gare_depart = Gares::where('name', $point_depart)->first();
+        $gare_distination = Gares::where('name', $point_distination)->first();
+
+        $voyage = Voyages::where('gare_depart', $gare_depart->id)
+            ->where('gare_arrivee', $gare_distination->id)
+            ->get();
+
+        $trainIds = $voyage->pluck('id');
+
+        $trains = Trains::whereIn('voyage_id', $trainIds)->get();
+
+        return view('Front-office.Reservations.trains', compact('voyage', 'trains', 'point_depart', 'point_distination'));
     }
 }
