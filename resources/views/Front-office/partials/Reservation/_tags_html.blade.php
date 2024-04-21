aa6bdd1ca82bc48112f7ee394a24e0218c7ec60e
ait ouaguerd anass <aitougeurdanass@gmail.com>
2024-04-21 14:13:59 +0100

    Add the footer to the home page here.

--- resources/views/Front-office/partials/Reservation/_tags_html.blade.php
+++ resources/views/Front-office/partials/Reservation/_tags_html.blade.php
@@ -9,12 +9,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="/assets/css/style2.css">
-
+    <link rel="stylesheet" href="/assets/css/reservations/ReservationStyle.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
 </head>
 
 <body>
 @yield('content')
+
 </body>
 
 
