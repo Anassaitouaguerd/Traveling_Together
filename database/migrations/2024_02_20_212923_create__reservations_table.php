b786b3801405933fc11b98ba990eb3a0dab56a65
ait ouaguerd anass <aitougeurdanass@gmail.com>
2024-04-24 19:46:58 +0100

    change the field (is_tecket) to (token_id)

--- database/migrations/2024_02_20_212923_create__reservations_table.php
+++ database/migrations/2024_02_20_212923_create__reservations_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->dateTime('date_reservation');
             $table->unsignedBigInteger('user_id');
-            $table->boolean('is_tecket')->default(false);
+            $table->string('token_id', 255);
             $table->unsignedBigInteger('place');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
