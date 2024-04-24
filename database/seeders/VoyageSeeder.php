d03e1631bb75ae38c9339f9de2e7510fc6d06ed0
ait ouaguerd anass <aitougeurdanass@gmail.com>
2024-04-24 19:51:05 +0100

    I'm still working on completing the seeder for the 'voyage' table.

---
+++ database/seeders/VoyageSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class VoyageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $voyage = [
+            [
+                "prix" => "160",
+                ""
+            ]
+        ];
+    }
+}
