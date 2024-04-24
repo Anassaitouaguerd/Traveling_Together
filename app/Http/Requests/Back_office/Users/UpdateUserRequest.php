cc927d0e8023f036aa4fb11df350e52851533354
ait ouaguerd anass <aitougeurdanass@gmail.com>
2024-04-24 19:42:13 +0100

    change te name space to (namespace App\Http\Requests\Back_office\Users;)

--- app/Http/Requests/Back_office/Users/UpdateUserRequest.php
+++ app/Http/Requests/Back_office/Users/UpdateUserRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Back_office\Users;
 
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -11,7 +11,7 @@ class UpdateUserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class UpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name" => "required",
+            'image' => 'required|image|mimes:png,jpg,jpeg',
+            "tel" => "required",
+            "email" => "required|email",
         ];
     }
 }
