d0f22bdbc79eccb7085123c5cc9d8fc9b508f749
ait ouaguerd anass <aitougeurdanass@gmail.com>
2024-04-25 10:39:15 +0100

    middleware for check the  users IF AUTHENTICABLE

---
+++ app/Http/Middleware/CheckAuth.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user_id = session("user_id");
+
+        if (!$user_id) return abort(401);
+
+        $user = User::where("id", $user_id)->first();
+
+        if (!$user) return abort(401);
+
+        return $next($request);
+    }
+}
